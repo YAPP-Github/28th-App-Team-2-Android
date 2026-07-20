70a9d46f9bba1ad347f070eb039cb81ec2b0137b
Moony-H <53536205+Moony-H@users.noreply.github.com>
2026-07-20 19:20:26 +0900

    [Style] 디자인 시스템 버튼 컴포넌트 추가
    * feat: add PrimaryButton and SecondaryButton components
    * feat: implement base TodakunButton with Material3 and custom theme mapping
    * feat: add Compose previews for primary, secondary, and disabled button states

---
+++ core/designsystem/src/main/java/com/kikidan/designsystem/component/button/TodakunButton.kt
@@ -0,0 +1,119 @@
+package com.kikidan.designsystem.component.button
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kikidan.designsystem.theme.TodakunColor
+import com.kikidan.designsystem.theme.TodakunTypography
+
+@Composable
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    TodakunButton(
+        text = text,
+        textColor = TodakunColor.primary700,
+        backgroundColor = TodakunColor.primary50,
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled
+    )
+}
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    TodakunButton(
+        text = text,
+        textColor = TodakunColor.white,
+        backgroundColor = TodakunColor.primary600,
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled
+    )
+}
+
+
+@Composable
+private fun TodakunButton(
+    text: String,
+    textColor: Color,
+    backgroundColor: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = textColor,
+            disabledContainerColor = TodakunColor.gray100,
+            disabledContentColor = TodakunColor.gray400
+        )
+    ) {
+        Text(
+            text = text,
+            style = TodakunTypography.body2Medium
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun PrimaryButtonPreview() {
+    PrimaryButton(
+        text = "다음 단계",
+        modifier = Modifier
+            .width(353.dp)
+            .height(52.dp),
+        enabled = true,
+        onClick = {}
+    )
+}
+
+
+@Composable
+@Preview
+private fun SecondaryButtonPreview() {
+    SecondaryButton(
+        text = "다음 단계",
+        modifier = Modifier
+            .width(353.dp)
+            .height(52.dp),
+        enabled = true,
+        onClick = {}
+    )
+}
+
+
+@Composable
+@Preview
+private fun DisableButtonPreview() {
+    SecondaryButton(
+        text = "다음 단계",
+        modifier = Modifier
+            .width(353.dp)
+            .height(52.dp),
+        enabled = false,
+        onClick = {}
+    )
+}
