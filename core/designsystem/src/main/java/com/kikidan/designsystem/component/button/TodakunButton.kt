1ae1573f9549137e062b2a9a86584d71086d965e
Moony-H <53536205+Moony-H@users.noreply.github.com>
2026-07-26 22:26:56 +0900

    [Style] 디자인 시스템 버튼 컴포넌트 기능 확장 및 고도화
    * feat: TodakunButton에 크기(Small, Medium, Large) 및 아이콘 배치 기능 추가
    * feat: 클릭 상호작용 시 커스텀 배경색 변경 처리 및 기본 리플 효과 제거
    * feat: 아이콘 포함 여부 및 크기별 버튼 상태를 확인할 수 있는 Preview 추가

--- core/designsystem/src/main/java/com/kikidan/designsystem/component/button/TodakunButton.kt
+++ core/designsystem/src/main/java/com/kikidan/designsystem/component/button/TodakunButton.kt
@@ -1,25 +1,72 @@
 package com.kikidan.designsystem.component.button
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.kikidan.designsystem.theme.TodakunColor
 import com.kikidan.designsystem.theme.TodakunTypography
+
+enum class TodakunButtonIconPosition {
+    Start,
+    End,
+}
+
+enum class TodakunButtonSize(
+    val height: Dp,
+    val iconSize: Dp,
+    val iconSpacing: Dp,
+) {
+    Small(
+        height = 32.dp,
+        iconSize = 16.dp,
+        iconSpacing = 1.dp,
+    ),
+    Medium(
+        height = 44.dp,
+        iconSize = 20.dp,
+        iconSpacing = 2.dp,
+    ),
+    Large(
+        height = 52.dp,
+        iconSize = 24.dp,
+        iconSpacing = 2.dp,
+    ),
+}
 
 @Composable
 fun PrimaryButton(
     text: String,
     onClick: () -> Unit,
+    size: TodakunButtonSize,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    painter: Painter? = null,
+    contentDescription: String? = null,
+    iconPosition: TodakunButtonIconPosition = TodakunButtonIconPosition.Start,
 ) {
     TodakunButton(
         text = text,
@@ -28,6 +75,10 @@ fun PrimaryButton(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
+        painter = painter,
+        contentDescription = contentDescription,
+        size = size,
+        iconPosition = iconPosition,
     )
 }
 
@@ -35,8 +86,12 @@ fun PrimaryButton(
 fun SecondaryButton(
     text: String,
     onClick: () -> Unit,
+    size: TodakunButtonSize,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    painter: Painter? = null,
+    contentDescription: String? = null,
+    iconPosition: TodakunButtonIconPosition = TodakunButtonIconPosition.Start,
 ) {
     TodakunButton(
         text = text,
@@ -45,6 +100,10 @@ fun SecondaryButton(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
+        painter = painter,
+        contentDescription = contentDescription,
+        size = size,
+        iconPosition = iconPosition,
     )
 }
 
@@ -55,69 +114,152 @@ private fun TodakunButton(
     backgroundColor: Color,
     onClick: () -> Unit,
     enabled: Boolean,
+    size: TodakunButtonSize,
     modifier: Modifier = Modifier,
+    painter: Painter? = null,
+    contentDescription: String? = null,
+    iconPosition: TodakunButtonIconPosition = TodakunButtonIconPosition.Start,
 ) {
     val textStyle =
         if (enabled) TodakunTypography.body2SemiBold else TodakunTypography.body2Medium
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
 
-    Button(
-        modifier = modifier,
-        onClick = onClick,
-        enabled = enabled,
-        shape = RoundedCornerShape(12.dp),
-        colors =
-            ButtonDefaults.buttonColors(
-                containerColor = backgroundColor,
-                contentColor = textColor,
-                disabledContainerColor = TodakunColor.gray100,
-                disabledContentColor = TodakunColor.gray400,
-            ),
-    ) {
-        Text(
-            text = text,
-            style = textStyle,
-        )
+    val containerColor =
+        if (isPressed) {
+            TodakunColor.gray975.copy(alpha = 0.16f).compositeOver(backgroundColor)
+        } else {
+            backgroundColor
+        }
+
+    CompositionLocalProvider(LocalRippleConfiguration provides null) {
+        Button(
+            modifier = Modifier.height(size.height).then(modifier),
+            onClick = onClick,
+            enabled = enabled,
+            shape = RoundedCornerShape(12.dp),
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = containerColor,
+                    contentColor = textColor,
+                    disabledContainerColor = TodakunColor.gray100,
+                    disabledContentColor = TodakunColor.gray400,
+                ),
+            interactionSource = interactionSource,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(size.iconSpacing, Alignment.CenterHorizontally),
+            ) {
+                if (painter != null && iconPosition == TodakunButtonIconPosition.Start) {
+                    Icon(
+                        painter = painter,
+                        contentDescription = contentDescription,
+                        modifier = Modifier.size(size.iconSize),
+                    )
+                }
+                Text(
+                    text = text,
+                    style = textStyle,
+                )
+                if (painter != null && iconPosition == TodakunButtonIconPosition.End) {
+                    Icon(
+                        painter = painter,
+                        contentDescription = contentDescription,
+                        modifier = Modifier.size(size.iconSize),
+                    )
+                }
+            }
+        }
     }
 }
 
 @Composable
 @Preview
 private fun PrimaryButtonPreview() {
-    PrimaryButton(
-        text = "다음 단계",
-        modifier =
-            Modifier
-                .width(353.dp)
-                .height(52.dp),
-        enabled = true,
-        onClick = {},
-    )
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        TodakunButtonSize.entries.forEach { size ->
+            PrimaryButton(
+                text = "다음 단계",
+                modifier = Modifier.width(353.dp),
+                size = size,
+                enabled = true,
+                onClick = {},
+            )
+        }
+    }
 }
 
 @Composable
 @Preview
 private fun SecondaryButtonPreview() {
-    SecondaryButton(
-        text = "다음 단계",
-        modifier =
-            Modifier
-                .width(353.dp)
-                .height(52.dp),
-        enabled = true,
-        onClick = {},
-    )
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        TodakunButtonSize.entries.forEach { size ->
+            SecondaryButton(
+                text = "다음 단계",
+                modifier = Modifier.width(353.dp),
+                size = size,
+                enabled = true,
+                onClick = {},
+            )
+        }
+    }
 }
 
 @Composable
 @Preview
 private fun DisableButtonPreview() {
-    SecondaryButton(
-        text = "다음 단계",
-        modifier =
-            Modifier
-                .width(353.dp)
-                .height(52.dp),
-        enabled = false,
-        onClick = {},
-    )
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        TodakunButtonSize.entries.forEach { size ->
+            SecondaryButton(
+                text = "다음 단계",
+                modifier = Modifier.width(353.dp),
+                size = size,
+                enabled = false,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun PrimaryButtonIconPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        TodakunButtonIconPosition.entries.forEach { position ->
+            TodakunButtonSize.entries.forEach { size ->
+                PrimaryButton(
+                    text = "다음 단계",
+                    modifier = Modifier.width(353.dp),
+                    size = size,
+                    enabled = true,
+                    painter = ColorPainter(TodakunColor.white), // icon 추가 예정
+                    contentDescription = null,
+                    iconPosition = position,
+                    onClick = {},
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun SecondaryButtonIconPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        TodakunButtonIconPosition.entries.forEach { position ->
+            TodakunButtonSize.entries.forEach { size ->
+                SecondaryButton(
+                    text = "다음 단계",
+                    modifier = Modifier.width(353.dp),
+                    size = size,
+                    enabled = true,
+                    painter = ColorPainter(TodakunColor.primary700),
+                    contentDescription = null,
+                    iconPosition = position,
+                    onClick = {},
+                )
+            }
+        }
+    }
 }
