cf942749c0d7abaf666ae070278f4b04d1ec3cb9
oungsi2000 <oungsi1000@gmail.com>
2026-07-31 21:07:30 +0900

    feat: 회원가입 및 로그아웃 API 구현
    RemoteAuthDataSource에 postSignup, postLogout을 추가한다.
    Ktor HttpResponse의 CommonResponse data 필드를 검증하고 반환하는 bodyNotNull() 확장 함수를 도입하여 기존 로그인 및 토큰 갱신 로직의 중복된 null 체크 코드를 제거한다.

--- core/data/src/main/java/com/kikidan/data/datasource/RemoteAuthDataSource.kt
+++ core/data/src/main/java/com/kikidan/data/datasource/RemoteAuthDataSource.kt
@@ -3,9 +3,16 @@ package com.kikidan.data.datasource
 import com.kikidan.domain.model.auth.AuthToken
 import com.kikidan.domain.model.auth.LoginResult
 import com.kikidan.domain.model.auth.OAuthCredential
+import com.kikidan.domain.model.auth.OAuthToken
+import com.kikidan.domain.model.auth.OnboardingToken
+import com.kikidan.domain.model.user.User
 
 interface RemoteAuthDataSource {
     suspend fun postLogin(oauthCredential: OAuthCredential): LoginResult
+
+    suspend fun postSignup(user: User, onboardingToken: OnboardingToken): AuthToken
+
+    suspend fun postLogout()
 
     suspend fun postRefresh(refreshToken: String): AuthToken
 }
