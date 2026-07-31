ee444c722106f5717944b52c8fcbeb4eb24d8479
oungsi2000 <oungsi1000@gmail.com>
2026-07-31 21:12:24 +0900

    chore: formatting ktlint

--- core/data-remote/src/main/java/com/kikidan/data_remote/datasource/RemoteAuthDataSourceImpl.kt
+++ core/data-remote/src/main/java/com/kikidan/data_remote/datasource/RemoteAuthDataSourceImpl.kt
@@ -6,9 +6,9 @@ import com.kikidan.data_remote.dto.auth.LoginRequest
 import com.kikidan.data_remote.dto.auth.LoginResponse
 import com.kikidan.data_remote.dto.auth.RefreshRequest
 import com.kikidan.data_remote.dto.auth.RefreshResponse
-import com.kikidan.data_remote.dto.auth.SignupRequest
 import com.kikidan.data_remote.dto.auth.SignupResponse
 import com.kikidan.data_remote.dto.auth.toDomain
+import com.kikidan.data_remote.dto.auth.toSignupRequest
 import com.kikidan.data_remote.util.bodyNotNull
 import com.kikidan.domain.model.auth.AuthToken
 import com.kikidan.domain.model.auth.LoginResult
@@ -24,56 +24,55 @@ import io.ktor.client.request.setBody
 import javax.inject.Inject
 
 class RemoteAuthDataSourceImpl
-@Inject
-constructor(
-    private val client: Lazy<HttpClient>,
-) : RemoteAuthDataSource {
-    override suspend fun postLogin(oauthCredential: OAuthCredential): LoginResult =
-        client
-            .get()
-            .post(LOGIN_URL) {
-                setBody(
-                    LoginRequest(
-                        provider = oauthCredential.provider.toString(),
-                        oauthAccessToken = oauthCredential.token.value,
-                    ),
-                )
-            }.bodyNotNull<LoginResponse>().toDomain()
+    @Inject
+    constructor(
+        private val client: Lazy<HttpClient>,
+    ) : RemoteAuthDataSource {
+        override suspend fun postLogin(oauthCredential: OAuthCredential): LoginResult =
+            client
+                .get()
+                .post(LOGIN_URL) {
+                    setBody(
+                        LoginRequest(
+                            provider = oauthCredential.provider.toString(),
+                            oauthAccessToken = oauthCredential.token.value,
+                        ),
+                    )
+                }.bodyNotNull<LoginResponse>()
+                .toDomain()
 
+        override suspend fun postSignup(
+            user: User,
+            onboardingToken: OnboardingToken,
+        ): AuthToken =
+            client
+                .get()
+                .post(SIGNUP_URL) {
+                    setBody(user.toSignupRequest(onboardingToken))
+                }.bodyNotNull<SignupResponse>()
+                .toDomain()
 
-    override suspend fun postSignup(user: User, onboardingToken: OnboardingToken): AuthToken = client
-        .get()
-        .post(SIGNUP_URL) {
-            setBody(
-                SignupRequest(
+        override suspend fun postLogout() {
+            client
+                .get()
+                .post(LOGOUT_URL)
+                .body<CommonResponse<Unit>>()
+        }
 
-                ),
-            )
-        }.bodyNotNull<SignupResponse>().toDomain()
+        override suspend fun postRefresh(refreshToken: String): AuthToken =
+            client
+                .get()
+                .post(REFRESH_URL) {
+                    // 설정하지 않았을 때, refreshToken이 두 번 실행
+                    attributes.put(AuthCircuitBreaker, Unit)
+                    setBody(RefreshRequest(refreshToken))
+                }.bodyNotNull<RefreshResponse>()
+                .toDomain()
 
-
-    override suspend fun postLogout() {
-        client
-            .get()
-            .post(LOGOUT_URL)
-            .body<CommonResponse<Unit>>()
+        companion object {
+            private const val REFRESH_URL = "api/v1/auth/refresh"
+            private const val LOGIN_URL = "api/v1/auth/login"
+            private const val SIGNUP_URL = "api/v1/auth/signup"
+            private const val LOGOUT_URL = "api/v1/auth/logout"
+        }
     }
-
-    override suspend fun postRefresh(refreshToken: String): AuthToken = client
-        .get()
-        .post(REFRESH_URL) {
-            // 설정하지 않았을 때, refreshToken이 두 번 실행
-            attributes.put(AuthCircuitBreaker, Unit)
-            setBody(RefreshRequest(refreshToken))
-        }.bodyNotNull<RefreshResponse>().toDomain()
-
-
-    companion object {
-        private const val REFRESH_URL = "api/v1/auth/refresh"
-        private const val LOGIN_URL = "api/v1/auth/login"
-        private const val SIGNUP_URL = "api/v1/auth/signup"
-
-        private const val LOGOUT_URL = "api/v1/auth/logout"
-
-    }
-}
