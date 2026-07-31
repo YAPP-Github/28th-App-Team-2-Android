564c31c7b06ed9d84bf28978673398956119b41a
Moony-H <53536205+Moony-H@users.noreply.github.com>
2026-07-31 18:33:07 +0900

    apply feature convention plugin to mypage module

--- feature/mypage/build.gradle.kts
+++ feature/mypage/build.gradle.kts
@@ -1,31 +1,7 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("todakun.feature")
 }
 
 android {
     namespace = "com.kikidan.mypage"
-    compileSdk {
-        version = release(37)
-    }
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-}
-
-dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
 }
