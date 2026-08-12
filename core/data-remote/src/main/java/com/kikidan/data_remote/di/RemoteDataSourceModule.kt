38f755a301539f93c238a306e026e061a0f637d3
oungsi2000 <oungsi1000@gmail.com>
2026-08-13 01:41:42 +0900

    refactor: RemoteDataSourceModule import 순서 정리
    RemoteDataSourceModule 내 데이터 소스 관련 import 문을 알파벳 순으로 정렬한다.

--- core/data-remote/src/main/java/com/kikidan/data_remote/di/RemoteDataSourceModule.kt
+++ core/data-remote/src/main/java/com/kikidan/data_remote/di/RemoteDataSourceModule.kt
@@ -2,14 +2,14 @@ package com.kikidan.data_remote.di
 
 import com.kikidan.data.auth.AuthTokenCacheInvalidator
 import com.kikidan.data.datasource.RemoteAuthDataSource
-import com.kikidan.data.datasource.RemoteDayFortuneDataSource
 import com.kikidan.data.datasource.RemoteChatDataSource
+import com.kikidan.data.datasource.RemoteDayFortuneDataSource
 import com.kikidan.data.datasource.RemoteFortuneDataSource
 import com.kikidan.data.datasource.RemoteLuckActionDataSource
 import com.kikidan.data_remote.auth.BearerTokenCacheInvalidator
 import com.kikidan.data_remote.datasource.RemoteAuthDataSourceImpl
-import com.kikidan.data_remote.datasource.RemoteDayFortuneDataSourceImpl
 import com.kikidan.data_remote.datasource.RemoteChatDataSourceImpl
+import com.kikidan.data_remote.datasource.RemoteDayFortuneDataSourceImpl
 import com.kikidan.data_remote.datasource.RemoteFortuneDataSourceImpl
 import com.kikidan.data_remote.datasource.RemoteLuckActionDataSourceImpl
 import dagger.Binds
